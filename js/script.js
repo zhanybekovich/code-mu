7db93babdbd03072fcbbc7b845db037a465319c9
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2025-02-02 11:04:30 +0600

    Change value of the variable

--- js/script.js
+++ js/script.js
@@ -1,18 +1,7 @@
 "use strict";
 
-let a = 1;
-let b = 2;
-let c = 3;
-
+let a = 10;
 alert(a);
-alert(b);
-alert(c);
 
-let d, e, f;
-d = 4;
-e = 5;
-f = 6;
-
-alert(d);
-alert(e);
-alert(f);
+a = 20;
+alert(a);
