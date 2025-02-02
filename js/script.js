da4dbd8eccc8cc97a2e29b5b279f826dea23635d
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2025-02-02 11:03:26 +0600

    Declare multiple variables

--- js/script.js
+++ js/script.js
@@ -1,4 +1,18 @@
 "use strict";
 
-let num = 123;
-alert(num);
+let a = 1;
+let b = 2;
+let c = 3;
+
+alert(a);
+alert(b);
+alert(c);
+
+let d, e, f;
+d = 4;
+e = 5;
+f = 6;
+
+alert(d);
+alert(e);
+alert(f);
