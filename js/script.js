5d444662da81cadb88a246179458adfe96d02917
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2025-02-04 21:47:31 +0600

    Document.write()

--- js/script.js
+++ js/script.js
@@ -1,10 +1,6 @@
 "use strict";
 
-// prompt
-let userName = prompt("What's your name?");
-console.log(userName);
+// document.write
+document.write(123);
 
-// prompt returns data as string
-let a = prompt("Enter 1st number");
-let b = prompt("Enter 2nd number");
-console.log(`The sum of ${a} + ${b} = ${a + b}`);
+document.write("<h1>Hello</h1>");
