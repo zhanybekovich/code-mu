6493f9c01eb7727a3792c15fb84366f5aacc4031
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2025-02-04 21:39:49 +0600

    Increment, decrement

--- js/script.js
+++ js/script.js
@@ -1,15 +1,21 @@
 "use strict";
 
-// assignment operations
-let num = 1;
-num += 3;
+// increment postfix
+let num = 0;
+console.log(num++);
 console.log(num);
 
-num -= 1;
-console.log(num);
+// increment prefix
+let num2 = 0;
+console.log(++num2);
+console.log(num2);
 
-num *= 2;
-console.log(num);
+// decrement postfix
+let num3 = 5;
+console.log(num3--);
+console.log(num3);
 
-num /= 2;
-console.log(num);
+// decrement prefix
+let num4 = 50;
+console.log(--num4);
+console.log(num4);
