d8efd7d280e48201474ecef831be58dbb4e5544a
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2025-02-04 19:10:49 +0600

    Type coercion

--- js/script.js
+++ js/script.js
@@ -1,13 +1,9 @@
 "use strict";
 
-// constants
-
-const pi = 3.14;
-
-console.log(pi);
-
-// calculate length of circle
-const radius = 50;
-const length = 2 * pi * radius;
-
-console.log(length);
+// type coercion
+console.log("2" * "3");
+console.log("2" + "3");
+console.log("2" - "1");
+console.log("10" / "2");
+console.log("10" % "3");
+console.log("2" * "3s");
