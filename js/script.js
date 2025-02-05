566492262dd0a6e479c6b6adba2bc2a7a3551732
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2025-02-05 21:13:13 +0600

    Objects: get, add, set values by keys

--- js/script.js
+++ js/script.js
@@ -1,12 +1,28 @@
 "use strict";
 
-// objects
+// objects: getting values by keys
 
-// empty object
-let obj = {};
-
-// object with keys and values
-let pet = {
-  name: "Bob",
+let user = {
+  name: "John",
   age: 25,
 };
+
+console.log(user.name);
+console.log(user.age);
+
+// create object date with year, month, day keys
+let date = {
+  year: 2025,
+  month: 2,
+  day: 5,
+};
+
+console.log(`${date.year}-${date.month}-${date.day}`);
+
+// change value by key
+date.year = 2026;
+console.log(date);
+
+// adding key and value
+date.time = "20:00";
+console.log(date);
