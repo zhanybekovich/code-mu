73efe1cd4e87ef2a0406d460b00186e73178fc94
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2025-02-05 20:56:59 +0600

    Creating arrays

--- js/script.js
+++ js/script.js
@@ -1,41 +1,18 @@
 "use strict";
 
-// Practice on operations
+// arrays
 
-// 1
-let secondsInADay = 24 * 60 * 60;
-console.log(secondsInADay);
+// create empty array
+let arr = [];
 
-// 2
-let days = 30;
-let secondsIn30Days = 30 * secondsInADay;
-console.log(secondsIn30Days);
+// array with strings
+let letters = ["a", "b", "c"];
 
-// 3
-let year = 365;
-let secondsInAYear = year * secondsInADay;
-console.log(secondsInAYear);
+// array with numbers
+let numbers = [1, 2, 3];
 
-// 4
-let hoursInADay = 24;
-let minutesInADay = hoursInADay * 60;
-console.log(minutesInADay);
+// array with combination of different types
+let combo = [1, 2, 3, "b", "c", null, true, false];
 
-// 5
-let minutesInAYear = 365 * minutesInADay;
-console.log(minutesInAYear);
-
-// 6
-let megaBytes = 2;
-let bytesInMegaByte = megaBytes * 1024 * 1024;
-console.log(bytesInMegaByte);
-
-// 7
-let gigaBytes = 2;
-let bytesInGigaBytes = gigaBytes * 1024 * 1024 * 1024;
-console.log(bytesInGigaBytes);
-
-// 8
-let terraBytes = 2;
-let bytesInTerraBytes = terraBytes * 1024 * 1024 * 1024 * 1024;
-console.log(bytesInTerraBytes);
+alert(letters);
+console.log(letters);
