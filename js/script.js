42104ac2cb64fedc3fe7f7bcc2ece129375c3831
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2025-02-05 17:21:42 +0600

    Tasks on fixing errors

--- js/script.js
+++ js/script.js
@@ -1,6 +1,46 @@
 "use strict";
 
-// document.write
-document.write(123);
+// tasks: fixing errors
 
-document.write("<h1>Hello</h1>");
+// 1
+let num1 = 1;
+let num2 = 2;
+console.log(`The sum is: ${num1 + num2}`);
+
+// 2
+let a = 1;
+let b = 2;
+console.log(a + b);
+
+// 3
+let num = "123";
+let sum = Number(num[0]) + Number(num[1]) + Number(num[2]);
+console.log(sum);
+
+// 4
+num = String(123);
+console.log(num[0]);
+
+// 5
+a = 0;
+console.log(++a);
+
+// 6
+num = 123;
+console.log(String(num).length);
+
+// 7
+a = 24 * 60 * 60;
+console.log(a);
+
+// 8
+num = 123;
+let str = String(num);
+console.log(str[str.length - 1]);
+
+// 9
+a = "123";
+b = "456";
+let s = Number(a) + Number(b);
+
+console.log(s);
