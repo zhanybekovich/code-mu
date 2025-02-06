d3a4b5cebb161b0b82adeb4ab2c65147d656d8c1
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2025-02-06 18:19:25 +0600

    Array of object keys

--- js/script.js
+++ js/script.js
@@ -1,28 +1,6 @@
 "use strict";
 
-// objects: getting values by keys
-
-let user = {
-  name: "John",
-  age: 25,
-};
-
-console.log(user.name);
-console.log(user.age);
-
-// create object date with year, month, day keys
-let date = {
-  year: 2025,
-  month: 2,
-  day: 5,
-};
-
-console.log(`${date.year}-${date.month}-${date.day}`);
-
-// change value by key
-date.year = 2026;
-console.log(date);
-
-// adding key and value
-date.time = "20:00";
-console.log(date);
+// array of object keys
+let obj = { a: 1, b: 2, c: 3 };
+let keys = Object.keys(obj);
+console.log(keys);
