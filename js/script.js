f23696f8158b32eb027c9e15ff901e5905d289bd
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2025-02-06 21:34:45 +0600

    Else if practice

--- js/script.js
+++ js/script.js
@@ -1,14 +1,15 @@
 "use strict";
 
-// else if
-let num = 1;
+// else if practice
 
-if (num == 1) {
-  console.log("value1");
-} else if (num == 2) {
-  console.log("value2");
-} else if (num == 3) {
-  console.log("value3");
+let day = 55;
+
+if (day >= 1 && day <= 10) {
+  console.log(1);
+} else if (day >= 11 && day <= 20) {
+  console.log(2);
+} else if (day >= 20 && day <= 31) {
+  console.log(3);
 } else {
-  console.log("неверное значение переменной num");
+  console.log("Day should be between 1 and 31");
 }
