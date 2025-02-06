c6ccc955cfebb8919eaea99f87acc52e5bd68fb7
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2025-02-06 21:30:56 +0600

    Else if

--- js/script.js
+++ js/script.js
@@ -1,34 +1,14 @@
 "use strict";
 
-// logical operators
-let num = 3;
+// else if
+let num = 1;
 
-// AND
-if (num > 0 && num < 10) {
-  console.log("Yes");
+if (num == 1) {
+  console.log("value1");
+} else if (num == 2) {
+  console.log("value2");
+} else if (num == 3) {
+  console.log("value3");
 } else {
-  console.log("No");
+  console.log("неверное значение переменной num");
 }
-
-// OR
-let num1 = 10;
-let num2 = -5;
-
-if (num1 > 0 || num2 > 0) {
-  console.log("+++");
-} else {
-  console.log("---");
-}
-
-// && has higher priority than ||
-num = 3;
-
-if ((num > 0 && num < 5) || (num > 10 && num < 20)) {
-  console.log("+++");
-} else {
-  console.log("---");
-}
-
-// ! -> NOT
-let isAdult = true;
-console.log(!isAdult);
