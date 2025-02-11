645529e2e8b37f4b0f4e278e73ff5b4e162857ff
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2025-02-11 22:05:33 +0600

    Practice

--- js/script.js
+++ js/script.js
@@ -1,35 +1,22 @@
 "use strict";
 
-// practice
+// В переменной arr содержится некоторый массив с числами. Напишите условие, которое проверит, что в массиве 3 элемента. Если это так, выведите на экран сумму элементов массива.
 
-const arr = [1, 5, 20];
+// const arr = [1, 2, 3];
 
-if (arr.length === 3) {
-  console.log(arr[0] + arr[1] + arr[2]);
+// if (arr.length === 3) {
+//   console.log(arr.reduce((acc, x) => acc + x, 0));
+// } else {
+//   console.log("Array has less or more than 3 elements");
+// }
+
+// Код должен проверить сумму чисел:
+
+let num1 = 1;
+let num2 = 2;
+
+if (num1 + num2 === 3) {
+  console.log("+++");
 } else {
-  console.log("Less or more than 3 elements");
-}
-
-const str = "abc";
-
-if (str.startsWith("a")) {
-  console.log("yes");
-} else {
-  console.log("no");
-}
-
-const str2 = "zyx";
-
-if (str2.endsWith("x")) {
-  console.log("x");
-} else {
-  console.log("not x");
-}
-
-const str3 = "abc";
-
-if (str3.startsWith("a") || str3.startsWith("b")) {
-  console.log("a or b");
-} else {
-  console.log("not a and b");
+  console.log("---");
 }
