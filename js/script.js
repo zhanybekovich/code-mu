c0e46250c757c02fbda8f7cfb5bb0a0c99895622
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2025-02-12 11:48:24 +0600

    For-of loop

--- js/script.js
+++ js/script.js
@@ -1,22 +1,11 @@
 "use strict";
 
-// В переменной arr содержится некоторый массив с числами. Напишите условие, которое проверит, что в массиве 3 элемента. Если это так, выведите на экран сумму элементов массива.
+// for of loop
 
-// const arr = [1, 2, 3];
+// Выведите в консоль все элементы следующего массива:
 
-// if (arr.length === 3) {
-//   console.log(arr.reduce((acc, x) => acc + x, 0));
-// } else {
-//   console.log("Array has less or more than 3 elements");
-// }
+let arr = ["a", "b", "c", "d", "e"];
 
-// Код должен проверить сумму чисел:
-
-let num1 = 1;
-let num2 = 2;
-
-if (num1 + num2 === 3) {
-  console.log("+++");
-} else {
-  console.log("---");
+for (let item of arr) {
+  console.log(item);
 }
